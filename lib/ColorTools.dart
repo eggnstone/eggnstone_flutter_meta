60b8a36ea4182aa6d2b97ec9bbbf16714e61b6b0
Mark Eggenstein <>
2020-02-18 10:46:55 +0100

    Added ColorTools.

---
+++ lib/ColorTools.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+
+class ColorTools
+{
+    static Color changeLuminance(Color color, double factor)
+    {
+        assert(factor >= 0 && factor <= 2);
+
+        final hsl = HSLColor.fromColor(color);
+        final hslDark = hsl.withLightness((hsl.lightness * factor).clamp(0.0, 1.0));
+
+        return hslDark.toColor();
+    }
+
+    static MaterialColor createMaterialColor(Color color)
+    {
+        if (color == null)
+            return null;
+
+        return MaterialColor(
+            color.value,
+            <int, Color>{
+                50: changeLuminance(color, 1.74),
+                100: changeLuminance(color, 1.59),
+                200: changeLuminance(color, 1.425),
+                300: changeLuminance(color, 1.251),
+                400: changeLuminance(color, 1.125),
+                500: color,
+                600: changeLuminance(color, 0.94),
+                700: changeLuminance(color, 0.85),
+                800: changeLuminance(color, 0.77),
+                900: changeLuminance(color, 0.63),
+            }
+        );
+
+        /*
+        final MaterialColor materialColor = MaterialColor(
+            color.value,
+            <int, Color>{
+                50: Color(0xFFE3F2FD), //  227 242 253     205  10  99
+                100: Color(0xFFBBDEFB), //  187 222 251     207  25  98
+                200: Color(0xFF90CAF9), //  144 202 249     206  42  97
+                300: Color(0xFF64B5F6), //  100 181 246     206  59  96
+                400: Color(0xFF42A5F5), //   66 165 245     206  73  96
+                500: Color(0xFF2196F3), //   33 150 243     206  86  95 base
+                600: Color(0xFF1E88E5), //   30 136 229     208  86  89
+                700: Color(0xFF1976D2), //   25 118 210     209  88  82
+                800: Color(0xFF1565C0), //   21 101 192     211  89  75
+                900: Color(0xFF0D47A1), //   13  71 161     216  91  63
+            },
+        );
+
+        Color c500 = Colors.blue.shade500;
+        print("");
+
+        Color c50 = Colors.blue.shade50;
+        print('c50:  ' + c50.toString() + " " + c50.red.toString() + " " + c50.green.toString() + " " + c50.blue.toString() + " " + HSLColor
+            .fromColor(c50)
+            .lightness
+            .toString());
+        Color d50 = changeLuminance(c500, 1.74);
+        print('d50:  ' + d50.toString() + " " + d50.red.toString() + " " + d50.green.toString() + " " + d50.blue.toString() + " " + HSLColor
+            .fromColor(d50)
+            .lightness
+            .toString());
+
+        Color c100 = Colors.blue.shade100;
+        print('c100: ' + c100.toString() + " " + c100.red.toString() + " " + c100.green.toString() + " " + c100.blue.toString() + " " + HSLColor
+            .fromColor(c100)
+            .lightness
+            .toString());
+        Color d100 = changeLuminance(c500, 1.59);
+        print('d100: ' + d100.toString() + " " + d100.red.toString() + " " + d100.green.toString() + " " + d100.blue.toString() + " " + HSLColor
+            .fromColor(d100)
+            .lightness
+            .toString());
+
+        Color c200 = Colors.blue.shade200;
+        print('c200: ' + c200.toString() + " " + c200.red.toString() + " " + c200.green.toString() + " " + c200.blue.toString() + " " + HSLColor
+            .fromColor(c200)
+            .lightness
+            .toString());
+        Color d200 = changeLuminance(c500, 1.425);
+        print('d200: ' + d200.toString() + " " + d200.red.toString() + " " + d200.green.toString() + " " + d200.blue.toString() + " " + HSLColor
+            .fromColor(d200)
+            .lightness
+            .toString());
+
+        Color c300 = Colors.blue.shade300;
+        print('c300: ' + c300.toString() + " " + c300.red.toString() + " " + c300.green.toString() + " " + c300.blue.toString() + " " + HSLColor
+            .fromColor(c300)
+            .lightness
+            .toString());
+        Color d300 = changeLuminance(c500, 1.251);
+        print('d300: ' + d300.toString() + " " + d300.red.toString() + " " + d300.green.toString() + " " + d300.blue.toString() + " " + HSLColor
+            .fromColor(d300)
+            .lightness
+            .toString());
+
+        Color c400 = Colors.blue.shade400;
+        print('c400: ' + c400.toString() + " " + c400.red.toString() + " " + c400.green.toString() + " " + c400.blue.toString() + " " + HSLColor
+            .fromColor(c400)
+            .lightness
+            .toString());
+        Color d400 = changeLuminance(c500, 1.125);
+        print('d400: ' + d400.toString() + " " + d400.red.toString() + " " + d400.green.toString() + " " + d400.blue.toString() + " " + HSLColor
+            .fromColor(d400)
+            .lightness
+            .toString());
+
+        print('c500: ' + c500.toString() + " " + c500.red.toString() + " " + c500.green.toString() + " " + c500.blue.toString() + " " + HSLColor
+            .fromColor(c500)
+            .lightness
+            .toString());
+
+        Color c600 = Colors.blue.shade600;
+        print('c600: ' + c600.toString() + " " + c600.red.toString() + " " + c600.green.toString() + " " + c600.blue.toString() + " " + HSLColor
+            .fromColor(c600)
+            .lightness
+            .toString());
+        Color d600 = changeLuminance(c500, 0.94);
+        print('d600: ' + d600.toString() + " " + d600.red.toString() + " " + d600.green.toString() + " " + d600.blue.toString() + " " + HSLColor
+            .fromColor(d600)
+            .lightness
+            .toString());
+
+        Color c700 = Colors.blue.shade700;
+        print('c700: ' + c700.toString() + " " + c700.red.toString() + " " + c700.green.toString() + " " + c700.blue.toString() + " " + HSLColor
+            .fromColor(c700)
+            .lightness
+            .toString());
+        Color d700 = changeLuminance(c500, 0.85);
+        print('d700: ' + d700.toString() + " " + d700.red.toString() + " " + d700.green.toString() + " " + d700.blue.toString() + " " + HSLColor
+            .fromColor(d700)
+            .lightness
+            .toString());
+
+        Color c800 = Colors.blue.shade800;
+        print('c800: ' + c800.toString() + " " + c800.red.toString() + " " + c800.green.toString() + " " + c800.blue.toString() + " " + HSLColor
+            .fromColor(c800)
+            .lightness
+            .toString());
+        Color d800 = changeLuminance(c500, 0.77);
+        print('d800: ' + d800.toString() + " " + d800.red.toString() + " " + d800.green.toString() + " " + d800.blue.toString() + " " + HSLColor
+            .fromColor(d800)
+            .lightness
+            .toString());
+
+        Color c900 = Colors.blue.shade900;
+        print('c800: ' + c900.toString() + " " + c900.red.toString() + " " + c900.green.toString() + " " + c900.blue.toString() + " " + HSLColor
+            .fromColor(c900)
+            .lightness
+            .toString());
+        Color d900 = changeLuminance(c500, 0.63);
+        print('d900: ' + d900.toString() + " " + d900.red.toString() + " " + d900.green.toString() + " " + d900.blue.toString() + " " + HSLColor
+            .fromColor(d900)
+            .lightness
+            .toString());
+        */
+    }
+}
--- lib/MetaThemeData.dart
+++ lib/MetaThemeData.dart
@@ -1,53 +1,57 @@
 import 'package:flutter/material.dart';
 
+import 'ColorTools.dart';
 import 'Meta.dart';
 
 class MetaThemeData
 {
     static ThemeData createMaterialTheme({
-        @required Color color,
         @required bool createDarkTheme,
+        @required Color color,
         TextStyle textStyleBody1,
         TextStyle textStyleButton,
         TextStyle textStyleSubhead,
     })
     {
-        assert(color != null);
         assert(createDarkTheme != null);
 
-        final MaterialColor materialColor = MaterialColor(
-            color.value,
-            <int, Color>{
-                50: Color(0xFFE3F2FD),
-                100: Color(0xFFBBDEFB),
-                200: Color(0xFF90CAF9),
-                300: Color(0xFF64B5F6),
-                400: Color(0xFF42A5F5),
-                500: color,
-                600: Color(0xFF1E88E5),
-                700: Color(0xFF1976D2),
-                800: Color(0xFF1565C0),
-                900: Color(0xFF0D47A1),
-            },
+        return createMaterialThemeWithBrightness(
+            brightness: Meta.brightness == null ? createDarkTheme ? Brightness.dark : Brightness.light : Meta.brightness,
+            color: color,
+            textStyleBody1: textStyleBody1,
+            textStyleButton: textStyleButton,
+            textStyleSubhead: textStyleSubhead,
         );
+    }
+
+    static ThemeData createMaterialThemeWithBrightness({
+        @required Brightness brightness,
+        @required Color color,
+        TextStyle textStyleBody1,
+        TextStyle textStyleButton,
+        TextStyle textStyleSubhead,
+    })
+    {
+        assert(brightness != null);
+
+        final MaterialColor materialColor = ColorTools.createMaterialColor(color);
 
         return ThemeData(
-            brightness: Meta.brightness == null ? createDarkTheme ? Brightness.dark : Brightness.light : Meta.brightness,
+            brightness: brightness,
 
-            //accentColor: Colors.red,
-            //buttonColor: Colors.red, //color, TODO: blue in dark mode
-
+            // TextField-Material base line in dark mode
+            accentColor: color,
             // text cursor
             cursorColor: color,
-            // AppBar in dark mode
+            // AppBar-Material in dark mode
             primaryColor: color,
-            //
+            // shade500 is primary color, shade600 is used for buttons in dark mode
             primarySwatch: materialColor,
             // text selection handles
             textSelectionHandleColor: color,
             // selected text
-            textSelectionColor: color.withOpacity(0.5),
-            // SwitchMaterial, Radio, Checkbox
+            textSelectionColor: color?.withOpacity(0.5),
+            // Switch-Material, Radio-Material, Checkbox-Material
             toggleableActiveColor: color,
 
             textTheme: TextTheme(
@@ -58,111 +62,3 @@ class MetaThemeData
         );
     }
 }
-
-/*
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-class MetaThemeData
-{
-    //final Color accentColorMaterial;
-    final Brightness brightness;
-
-    //final Color cursorColorMaterial;
-    final Color primaryColor;
-
-    //final Color primarySwatchMaterial;
-
-    //final Color textSelectionColorMaterial;
-    //final Color textSelectionHandleColorMaterial;
-    final TextStyle textStyleMaterialBody1;
-    final TextStyle textStyleMaterialButton;
-    final TextStyle textStyleMaterialSubhead;
-
-    /// 1 [toggleableActiveColorMaterial] Radio, Checkbox, AutoSwitchTile-Android
-    //final Color toggleableActiveColorMaterial;
-
-    //final bool useDarkMode;
-
-    /// 2 [toggleableActiveColorMaterial] Radio, Checkbox, AutoSwitchTile-Android
-    MetaThemeData({
-        //this.accentColorMaterial,
-        this.brightness,
-        //this.cursorColorMaterial,
-        this.primaryColor,
-        //this.primarySwatchMaterial,
-        //this.textSelectionColorMaterial,
-        //this.textSelectionHandleColorMaterial,
-        this.textStyleMaterialBody1,
-        this.textStyleMaterialButton,
-        this.textStyleMaterialSubhead,
-        // this.toggleableActiveColorMaterial,
-        //this.useDarkMode
-    });
-
-    CupertinoThemeData getThemeCupertino()
-    {
-        print('getThemeCupertino: brightness: $brightness');
-
-        //return MaterialBasedCupertinoThemeData();
-        return CupertinoThemeData(
-            brightness: brightness, //brightness, //useDarkMode == null ? null : useDarkMode ? Brightness.dark : Brightness.light,
-            //brightness: useDarkMode == null ? null : useDarkMode ? Brightness.dark : Brightness.light,
-            primaryColor: primaryColor,
-
-            */
-/*barBackgroundColor: primaryColor,
-            //scaffoldBackgroundColor: Colors.red,
-
-
-            primaryContrastingColor: accentColor,
-            textTheme: CupertinoTextThemeData(
-            ),*/ /*
-
-        );
-    }
-
-    //ThemeData getThemeMaterial({@required BuildContext context, @required bool createDarkTheme})
-    ThemeData getThemeMaterial({ @required bool createDarkTheme})
-    {
-        return createThemeMaterial(
-            brightness: brightness == null ? (createDarkTheme == true ? Brightness.dark : createDarkTheme == false ? Brightness.light : null) : brightness,
-            primaryColor: primaryColor,
-        );
-    }
-
-    static ThemeData createThemeMaterial({Brightness brightness, Color primaryColor})
-    {
-        return ThemeData(
-            brightness: brightness,
-
-            accentColor: primaryColor,
-            cursorColor: primaryColor,
-            primaryColor: primaryColor,
-            primarySwatch: createMaterialColor(primaryColor),
-            textSelectionColor: primaryColor.withOpacity(0.5),
-            textSelectionHandleColor: primaryColor,
-            toggleableActiveColor: primaryColor,
-        );
-    }
-
-    static MaterialColor createMaterialColor(Color color)
-    {
-        return MaterialColor(
-            color.value,
-            <int, Color>{
-                50: Color(0xFFE3F2FD),
-                100: Color(0xFFBBDEFB),
-                200: Color(0xFF90CAF9),
-                300: Color(0xFF64B5F6),
-                400: Color(0xFF42A5F5),
-                500: color,
-                600: Color(0xFF1E88E5),
-                700: Color(0xFF1976D2),
-                800: Color(0xFF1565C0),
-                900: Color(0xFF0D47A1),
-            },
-        );
-    }
-}
-*/
