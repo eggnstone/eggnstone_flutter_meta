0e45cb80b4c2d736e710bf963387839349f1d9e9
Mark Eggenstein <>
2020-02-19 18:32:16 +0100

    Added Meta.isDark().

--- lib/Meta.dart
+++ lib/Meta.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 import 'MetaDesign.dart';
 
@@ -14,4 +15,19 @@ class Meta
 
     static bool get isDesignMaterial
     => !Platform.isIOS || forceDesign == MetaDesign.Material;
+
+    static bool isDark(BuildContext context)
+    {
+        if (brightness == Brightness.light)
+            return false;
+
+        if (brightness == Brightness.dark)
+            return true;
+
+        Brightness platformBrightness = MediaQuery
+            .of(context)
+            .platformBrightness;
+
+        return platformBrightness == Brightness.dark;
+    }
 }
