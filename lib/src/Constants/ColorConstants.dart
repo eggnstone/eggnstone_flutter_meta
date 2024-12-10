8cea9185d3385f400d7da69161955e0208929b5e
Mark Eggenstein <mark.eggenstein@eggnstone.com>
2024-12-10 16:03:47 +0700

    Fixed MetaAlertDialogButton colors.

--- lib/src/Constants/ColorConstants.dart
+++ lib/src/Constants/ColorConstants.dart
@@ -4,7 +4,7 @@ class ColorConstants
 {
     static const MaterialColor DEBUG_COLOR = Colors.pink;
     static const MaterialColor ELEVATED_BUTTON__DISABLED_COLOR = Colors.grey;
-    static const Color TEXT_BUTTON__DARK_DESTRUCTIVE_FOREGROUND_COLOR = Color(0xffff2222);
+    static const Color TEXT_BUTTON__DARK_DESTRUCTIVE_FOREGROUND_COLOR = Color(0xffff3333);
     // ignore: non_constant_identifier_names
     static final Color TEXT_BUTTON__DARK_DISABLED_FOREGROUND_COLOR = Colors.grey.shade600;
     static const Color TEXT_BUTTON__NORMAL_LIGHT_FOREGROUND_COLOR = Color(0xff222222);
